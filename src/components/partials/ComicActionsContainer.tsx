17827f41a222eacdc459ca8adf744d6763838da8
leephan2k1 <pttl2k1@gmail.com>
2023-07-08 23:25:15 +0700

    style: 💄 increase width size

--- src/components/partials/ComicActionsContainer.tsx
+++ src/components/partials/ComicActionsContainer.tsx
@@ -8,14 +8,14 @@ import {
 export default function ComicActionsContainer() {
   return (
     <View className='fit w-full my-4'>
-      <View className='w-[80%] flex-col mx-auto'>
+      <View className='w-[85%] flex-col mx-auto'>
         <TouchableOpacity className='flex-row items-center justify-center space-x-2 w-full p-4 bg-primary rounded-xl'>
           <BookOpenRoundedIcon width={20} height={20} className='text-black' />
           <Text className='text-black font-bold text-base'>Đọc ngay</Text>
         </TouchableOpacity>
 
         <View className='flex-row items-center justify-between mt-4'>
-          <TouchableOpacity className='flex-row items-center justify-center border border-gray-500 rounded-xl p-4 w-[45%]'>
+          <TouchableOpacity className='mr-2 flex-row items-center justify-center border border-gray-500 rounded-xl p-4 w-[45%]'>
             <InfoSquareRoundedIcon
               width={20}
               height={20}
