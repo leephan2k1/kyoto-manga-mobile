d0d794d17c3af049de257026805a1a80b48e738a
leephan2k1 <pttl2k1@gmail.com>
2023-07-23 22:57:30 +0700

    feat: 🎸 handle render popular comics

--- src/components/partials/PopularSection.tsx
+++ src/components/partials/PopularSection.tsx
@@ -5,13 +5,20 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import PopularCard from '~/components/shared/PopularCard';
+import PopularCard, {
+  PopularCardSkeleton,
+} from '~/components/shared/PopularCard';
 import { useAppNavigation } from '~/hooks/navigationHooks';
+import { getPopulationsService } from '~/services/getPopulations.service';
+import { cluster } from 'radash';
+import { getDumpArray } from '~/utils/array.util';
 
 const { width } = Dimensions.get('window');
 
 export default function PopularSection() {
   const navigation = useAppNavigation();
+
+  const { data: populationsComic, status } = getPopulationsService();
 
   return (
     <View className='flex-col w-full h-fit px-4 py-2 mt-4'>
@@ -23,23 +30,66 @@ export default function PopularSection() {
         </TouchableOpacity>
       </View>
 
-      <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={[1, 2, 3]}
-        renderItem={({ item, index }) => {
-          return (
-            <View className={`flex-col mr-8`}>
-              <PopularCard style={{ width: (width * 80) / 100 }} isTop />
-              <PopularCard style={{ width: (width * 80) / 100 }} isTop />
-              <PopularCard
-                style={{ width: (width * 80) / 100 }}
-                isTop={false}
-              />
-            </View>
-          );
-        }}
-      />
+      {populationsComic && populationsComic.length > 0 && (
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={cluster(populationsComic.slice(0, 9), 3)}
+          renderItem={({ item, index }) => {
+            let order = index * 3;
+            if (item.length < 3) return null;
+
+            return (
+              <View className={`flex-col mr-8`}>
+                <PopularCard
+                  order={order + 1}
+                  comic={item[0]}
+                  style={{ width: (width * 80) / 100 }}
+                  isTop
+                />
+                <PopularCard
+                  order={order + 2}
+                  comic={item[1]}
+                  style={{ width: (width * 80) / 100 }}
+                  isTop
+                />
+                <PopularCard
+                  order={order + 3}
+                  comic={item[2]}
+                  style={{ width: (width * 80) / 100 }}
+                  isTop={false}
+                />
+              </View>
+            );
+          }}
+        />
+      )}
+
+      {status === 'loading' && (
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={getDumpArray(3)}
+          renderItem={({ item, index }) => {
+            return (
+              <View className={`flex-col mr-8`}>
+                <PopularCardSkeleton
+                  style={{ width: (width * 80) / 100 }}
+                  isTop
+                />
+                <PopularCardSkeleton
+                  style={{ width: (width * 80) / 100 }}
+                  isTop
+                />
+                <PopularCardSkeleton
+                  style={{ width: (width * 80) / 100 }}
+                  isTop={false}
+                />
+              </View>
+            );
+          }}
+        />
+      )}
     </View>
   );
 }
--- src/screens/PopularScreen.tsx
+++ src/screens/PopularScreen.tsx
@@ -1,34 +1,66 @@
 import { FlatList, StatusBar, Text, View } from 'react-native';
 import CustomSafeArea from '~/components/shared/CustomSafeArea';
 import BackHeader from '~/components/shared/BackHeader';
-import PopularCard from '~/components/shared/PopularCard';
+import PopularCard, {
+  PopularCardSkeleton,
+} from '~/components/shared/PopularCard';
 import PopularFilterModal from '~/components/partials/PopularFilterModal';
+import { getPopulationsService } from '~/services/getPopulations.service';
+import { getDumpArray } from '~/utils/array.util';
 
 export default function PopularScreen() {
+  const { data: populationsComic, status } = getPopulationsService();
+
   return (
-    <CustomSafeArea>
-      <View className='flex-1 bg-black px-2 py-4'>
+    <CustomSafeArea excludes={{ bottom: true }}>
+      <View className='flex-1 bg-black px-2 pt-4'>
         <StatusBar barStyle={'light-content'} />
 
         <BackHeader>
           <PopularFilterModal />
         </BackHeader>
 
-        <Text className='text-3xl text-bold text-white ml-2 my-4'>
+        <Text className='text-3xl text-bold text-white ml-2  mb-4'>
           Phổ biến
         </Text>
 
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-          renderItem={({ item, index }) => {
-            return (
-              <View className={`flex-col mr-8 w-full`}>
-                <PopularCard isTop style={{ width: '100%' }} />
-              </View>
-            );
-          }}
-        />
+        {populationsComic && populationsComic.length > 0 && (
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={populationsComic}
+            className='flex-1'
+            renderItem={({ item, index }) => {
+              return (
+                <View className={`flex-col mr-8 w-full`}>
+                  <PopularCard
+                    comic={item}
+                    order={index + 1}
+                    isTop={index < populationsComic?.length - 1}
+                    style={{ width: '100%' }}
+                  />
+                </View>
+              );
+            }}
+          />
+        )}
+
+        {status === 'loading' && (
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={getDumpArray(6)}
+            className='flex-1'
+            renderItem={({ item, index }) => {
+              return (
+                <View className={`flex-col mr-8 w-full`}>
+                  <PopularCardSkeleton
+                    isTop={index < 5}
+                    style={{ width: '100%' }}
+                  />
+                </View>
+              );
+            }}
+          />
+        )}
       </View>
     </CustomSafeArea>
   );
