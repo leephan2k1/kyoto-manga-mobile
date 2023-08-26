d129421872773b17cf736511ce316087afcf2bda
leephan2k1 <pttl2k1@gmail.com>
2023-08-26 23:44:17 +0700

    feat: 🎸 handle Infinite scroll

--- src/components/partials/ComicsList.tsx
+++ src/components/partials/ComicsList.tsx
@@ -1,4 +1,10 @@
-import { FlatList, TouchableOpacity } from 'react-native';
+import {
+  FlatList,
+  StyleProp,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
 import React, {
   ComponentType,
   JSXElementConstructor,
@@ -9,12 +15,23 @@ import ComicCard from '~/components/shared/ComicCard';
 import { CardLayout } from '~/common/types/cardLayouts';
 import { GarbageIcon } from '~/components/icons';
 import { mock_comic_preview } from '~/common/constants';
+import { ComicPreview } from '~/models/Comic';
+import Skeleton from '~/components/shared/Skeleton';
+import TorriGate from '~/components/icons/TorriGate';
 
 function ComicsList({
   listHeaderComp,
   cardLayout,
   deleteMode,
+  comics,
+  actionOnEndReachedProp,
+  isFetchingNextPage,
+  contentContainerStyle,
 }: {
+  contentContainerStyle?: StyleProp<ViewStyle>;
+  isFetchingNextPage?: boolean;
+  actionOnEndReachedProp?: () => void;
+  comics?: ComicPreview[];
   deleteMode?: boolean;
   cardLayout: CardLayout;
   listHeaderComp?:
@@ -23,19 +40,39 @@ function ComicsList({
     | null
     | undefined;
 }) {
+  const actionOnEndReached = () => {
+    if (actionOnEndReachedProp) {
+      actionOnEndReachedProp();
+    }
+  };
+
   if (cardLayout === 'list') {
     return (
       <FlatList
+        onEndReached={() => actionOnEndReached()}
+        ListFooterComponent={
+          isFetchingNextPage ? (
+            <View className='w-full h-20 flex items-center justify-center'>
+              <Skeleton height={50} width={50}>
+                <View className='bg-white/20 w-full h-full rounded-xl items-center justify-center flex'>
+                  <TorriGate width={30} height={30} />
+                </View>
+              </Skeleton>
+            </View>
+          ) : undefined
+        }
         ListHeaderComponent={listHeaderComp}
-        key={cardLayout}
+        key={cardLayout + '_list_'}
         style={{ borderRadius: 10 }}
         showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => cardLayout + item}
-        data={Array.from(new Array(15).keys()).reverse()}
+        keyExtractor={(item) => cardLayout + item.path}
+        data={
+          comics ? comics : Array.from(new Array(1).fill(mock_comic_preview))
+        }
         renderItem={({ item, index }) => {
           return (
             <ComicCard
-              comicPreview={mock_comic_preview}
+              comicPreview={item}
               CornerButton={
                 deleteMode ? (
                   <TouchableOpacity className='z-50 absolute bottom-0 right-2 bg-black/50 p-3 rounded-full'>
@@ -52,24 +89,40 @@ function ComicsList({
             />
           );
         }}
-      />
+      ></FlatList>
     );
   }
 
   return (
     <FlatList
       ListHeaderComponent={listHeaderComp}
-      key={cardLayout}
+      ListFooterComponent={
+        isFetchingNextPage ? (
+          <View className='w-full h-20 flex items-center justify-center'>
+            <Skeleton height={50} width={50}>
+              <View className='bg-white/20 w-full h-full rounded-xl items-center justify-center flex'>
+                <TorriGate width={30} height={30} />
+              </View>
+            </Skeleton>
+          </View>
+        ) : undefined
+      }
+      onEndReached={() => actionOnEndReached()}
+      key={cardLayout + '_layout_'}
       style={{ borderRadius: 10 }}
       showsVerticalScrollIndicator={false}
-      keyExtractor={(item) => cardLayout + item}
+      keyExtractor={(item) => cardLayout + item.path}
       numColumns={2}
-      data={Array.from(new Array(15).keys()).reverse()}
-      columnWrapperStyle={{ justifyContent: 'center', marginTop: 16 }}
+      data={comics ? comics : Array.from(new Array(1).fill(mock_comic_preview))}
+      columnWrapperStyle={{
+        gap: 20,
+        marginTop: 14,
+      }}
+      contentContainerStyle={contentContainerStyle}
       renderItem={({ item, index }) => {
         return (
           <ComicCard
-            comicPreview={mock_comic_preview}
+            comicPreview={item}
             CornerButton={
               deleteMode ? (
                 <TouchableOpacity className='z-50 absolute top-2 right-2 bg-black/50 p-3 rounded-full'>
@@ -78,7 +131,6 @@ function ComicsList({
               ) : undefined
             }
             cardLayout={cardLayout}
-            style={{ marginRight: index % 2 == 0 ? 22 : 0 }}
           />
         );
       }}
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,24 +1,85 @@
-import { Dimensions, TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import CustomSafeArea from '~/components/shared/CustomSafeArea';
 import { ThreeDotsIcon } from '~/components/icons';
 import SearchHeaderFilter from '~/components/partials/SearchHeaderFilter';
 import SearchInput from '~/components/shared/SearchInput';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import SearchModalFilters from '~/components/partials/SearchModalFilters';
 import LayoutFilters from '~/components/shared/LayoutFilters';
 import { CardLayout } from '~/common/types/cardLayouts';
+import { useSearchComic } from '~/services/searchComic.service';
 import ComicsList from '~/components/partials/ComicsList';
+import { ComicPreview } from '~/models/Comic';
+import Skeleton from '~/components/shared/Skeleton';
+import TorriGate from '~/components/icons/TorriGate';
+import { getFeaturedSortValue } from '~/helpers/getFeaturedSortValue';
+import { getLatestSortValue } from '~/helpers/getLatestSortValue';
+import { ISearchParams } from '~/common/interfaces/ISearchParams';
 
-const { width } = Dimensions.get('window');
+export type SearchSection = 'featured' | 'latest' | 'filter';
+
 export default function SearchScreen() {
   const [openSheetFilters, setOpenSheetFilter] = useState(false);
   const [cardLayout, setCardLayout] = useState<CardLayout>('grid');
+  const [searchKeyword, setSearchKeyword] = useState<string | undefined>('');
+  const [filters, setFilters] = useState<ISearchParams>({});
+  const [searchSection, setSearchSection] = useState<SearchSection>('featured');
+
+  const handleOnSubmit = (value: string) => {
+    setSearchKeyword(value);
+    setSearchSection('filter');
+  };
+
+  const sortValue = useMemo(() => {
+    setFilters((f) => ({ ...f, sort: undefined }));
+    let sort: string | undefined;
+
+    if (searchSection === 'featured') {
+      //TODO: update source dynamically
+      sort = getFeaturedSortValue('TruyenTranhLH');
+    } else if (searchSection === 'latest') {
+      sort = getLatestSortValue('TruyenTranhLH');
+    }
+
+    return sort;
+  }, [searchSection]);
+
+  const {
+    refetch,
+    data: comicsListResponse,
+    status,
+    fetchNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useSearchComic({
+    keyword: searchKeyword,
+    sort: filters.sort || sortValue,
+    status: filters.status,
+    genre: filters.genre,
+  });
+
+  const handleApplyFilter = () => {
+    refetch();
+    setOpenSheetFilter(false);
+    setSearchSection('filter');
+  };
+
+  const comics = useMemo(() => {
+    if (!comicsListResponse) return;
+
+    return comicsListResponse?.pages.reduce((acc: ComicPreview[], data) => {
+      return acc.concat(data.comics);
+    }, []);
+  }, [comicsListResponse, status]);
 
   return (
     <CustomSafeArea excludes={{ bottom: true }}>
       <View className='flex-1 p-2'>
         <View className='w-full items-center justify-between flex-row my-2'>
-          <SearchInput style={{ width: '60%' }} />
+          <SearchInput
+            handleOnSubmit={handleOnSubmit}
+            style={{ width: '60%' }}
+          />
 
           <View className='flex-row space-x-2'>
             <LayoutFilters
@@ -32,14 +93,41 @@ export default function SearchScreen() {
           </View>
         </View>
 
-        <SearchHeaderFilter setOpenSheetFilter={setOpenSheetFilter} />
+        <SearchHeaderFilter
+          searchSection={searchSection}
+          setSearchKeyword={setSearchKeyword}
+          setSearchSection={setSearchSection}
+          setOpenSheetFilter={setOpenSheetFilter}
+        />
 
         <SearchModalFilters
+          handleApplyFilter={handleApplyFilter}
+          setFilters={setFilters}
           open={openSheetFilters}
           setOpenSheet={setOpenSheetFilter}
         />
 
-        <ComicsList cardLayout={cardLayout} />
+        {status === 'loading' && isFetching && (
+          <View className='w-full flex-1 flex items-center justify-center'>
+            <Skeleton height={50} width={50}>
+              <View className='bg-white/20 w-full h-full rounded-xl items-center justify-center flex'>
+                <TorriGate width={30} height={30} />
+              </View>
+            </Skeleton>
+          </View>
+        )}
+
+        {status === 'success' && comics && comics.length > 0 && (
+          <ComicsList
+            isFetchingNextPage={isFetchingNextPage}
+            actionOnEndReachedProp={() => {
+              fetchNextPage();
+            }}
+            comics={comics}
+            cardLayout={cardLayout}
+            contentContainerStyle={{ paddingHorizontal: 10 }}
+          />
+        )}
       </View>
     </CustomSafeArea>
   );
